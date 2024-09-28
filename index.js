d46912357728d3fc2b5790a90ae23062943ca67d
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2024-09-28 21:56:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: "https://travello-front.vercel.app",  // Remove the square brackets
+  origin: "*",  // Remove the square brackets
   methods: ["GET", "POST", "PATCH"],
   credentials: true
 }));
