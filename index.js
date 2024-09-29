a442094a5957e450056e4781ae39adc273ac422b
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2024-09-29 08:54:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,12 +27,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: "*",  // Temporarily allow all origins for testing purposes
-  methods: ["GET", "POST", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],  // Ensure these headers are allowed
-  credentials: true
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
